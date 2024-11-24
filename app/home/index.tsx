df221d55a1c4259bd7bd780be0bfd5d3b4ded635
Sivianny Vega <rvegamor@ccss.sa.cr>
2024-11-23 18:36:29 -0600

    Pantalla de carga y SafeArea manual con hook safeArea

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,16 +1,23 @@
 import { useMovies } from '@/presentation/hooks/useMovies'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
 
+  const safeArea = useSafeAreaInsets()
   const { nowPlayingQuery } = useMovies()
 
+  if(nowPlayingQuery.isLoading){
+    return (
+      <View className='justify-center items-center flex-1'>
+        <ActivityIndicator color="#999999" size={30} />
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>
-        { JSON.stringify(nowPlayingQuery.data) }
-      </Text>
+    <View className='mt-2' style={{ paddingTop: safeArea.top }}>
+      <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
     </View>
   )
 }
