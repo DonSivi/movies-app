34927091b4cb2c07359dc8e2eb16a22a33eaba75
Sivianny Vega <rvegamor@ccss.sa.cr>
2024-11-23 18:24:34 -0600

    Instalación y Configuración de TanStack Query y prueba de petición inicial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { Stack } from 'expo-router';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import "../global.css";
 
 const RootLayout = () => {
+
+  // TanStackQuery: Cliente que se encargará de cachear todas las peticiones asíncronas
+  const queryClient = new QueryClient()
+
   return (
-    <View>
-      <Text className='text-3xl'>RootLayout</Text>
-    </View>
+    // TanStackQuery: Envoltorio de QueryClientProvider
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{
+        headerShown: false
+      }} />
+    </QueryClientProvider>
   )
 }
 
