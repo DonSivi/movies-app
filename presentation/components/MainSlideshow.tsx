3419223dc91249aa466e52f3087527f4be6d23e7
Sivianny Vega <rvegamor@ccss.sa.cr>
2024-11-25 07:42:12 -0600

    Creación de carrusel y ajustes al componente porque no era compatible con Expo en su versión más reciente

---
+++ presentation/components/MainSlideshow.tsx
@@ -0,0 +1,45 @@
+import { useWindowDimensions, View, Text } from 'react-native'
+import { Movie } from '@/infraestructure/interfaces/movie.interface'
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel'
+import { useRef } from 'react'
+
+interface Props{
+    movies: Movie[]
+}
+
+const MainSlideshow = ({movies}:Props) => {
+
+    // Necesario para el Carousel
+    const ref = useRef<ICarouselInstance>(null)
+
+    // Ancho del dispositivo así esté vertical u horizontal
+    const width = useWindowDimensions().width
+
+    return (
+        <View className='h-[250px] w-full'>
+            <Carousel 
+                ref={ref}
+                data={movies}
+                renderItem={ ({item}) => <Text>{item.title}</Text> } 
+                width={200}
+                height={350}
+                style={{
+                    width: width,
+                    height: 350,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                mode='parallax'
+                modeConfig={{
+                    // Esto no es necesario, es para saber que se puede modificar valores del "mode"
+                    parallaxScrollingScale: 0.9,
+                    parallaxScrollingOffset: 50
+                }}
+                // 0 sería el primer elemento, 1 el segundo, etc
+                defaultIndex={1}
+            />
+        </View>
+    )
+}
+
+export default MainSlideshow
